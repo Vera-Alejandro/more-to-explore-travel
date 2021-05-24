b2701226a6f34b94a7d95785c2aae190b0895d92
Vera-Alejandro <Alejandro.Vera-Gonzalez@trojans.dsu.edu>
2021-05-23 21:15:55 -0500

    Basic Button and nav bar

---
+++ src/components/Navigation.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-toolbar dark flat>
+    <v-toolbar-title>More To Explore Travel</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items v-for="item in nav" :key="item.icon" :title="item.title" >
+      <v-btn>{{ item.text }}</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data() {
+    return {
+      dialog: false,
+      nav: [
+        {
+          icon: "home",
+          text: "Home",
+          title: "Back to Home page",
+          active: true,
+        },
+        {
+          icon: "info",
+          text: "About",
+          title: "About this demo",
+          active: false,
+        },
+        {
+          icon: "assignment_turned_in",
+          text: "Todos",
+          title: "Some stuff that needs doing",
+          active: false,
+        },
+        {
+          icon: "email",
+          text: "Contact",
+          title: "Our Contact info",
+          active: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
