09a40fa28ce06bba8c750dbb1671be914df3a5b0
Vera-Alejandro <Alejandro.Vera-Gonzalez@trojans.dsu.edu>
2021-05-25 16:01:50 -0500

    form basics

--- src/components/Home/LetsGetInTouch.vue
+++ src/components/Home/LetsGetInTouch.vue
@@ -2,18 +2,66 @@
   <div class="container contact">
     <div>
       <h2>Let's Get in Touch!</h2>
+      <div>
+        <form>
+          <v-text-field
+            v-model="name"
+            :error-messages="nameErrors"
+            :counter="25"
+            label="Name"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+          ></v-text-field>
+          <v-select
+            v-model="message"
+            label="Message"
+            required
+            @change="$v.select.$touch()"
+            @blur="$v.select.$touch()"
+          ></v-select>
+
+          <v-btn class="mr-4" @click="submit"> submit </v-btn>
+          <v-btn @click="clear"> clear </v-btn>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-    name: 'LetsGetInTouch',
-    data () {
-        return {
+import { validationMixin } from 'vuelidate'
+import { required, maxLength, email } from 'vuelidate/lib/validators'
 
-        };
+export default {
+  mixins: [validationMixin],
+  validations: {
+    name: { required, maxLength: maxLength(25) },
+    email: { required, email },
+    message: { required },
+  },
+  name: "LetsGetInTouch",
+  components: {},
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+    };
+  },
+  methods: {
+    submit() {
+      this.$refs.observer.validate();
     },
+  },
 };
 </script>
 
