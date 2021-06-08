4822b4a2d2c68a00d08d8859dee61a1129897f9c
Alejandro Vera <averagonzalez@omnitech-inc.com>
2021-06-08 08:21:51 -0500

    update welcome text

--- src/components/About Me/Introduction.vue
+++ src/components/About Me/Introduction.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="intro-container">
-    <div class="text-cont">
-      <h2 class="local-title">{{ articleTitle }}</h2>
-      <article class="welcome-msg">
-        <p>{{ articleText }}</p>
-      </article>
-    </div>
-
-    <div class="img-cont">
-      <aside class="profile-picture">
-        <img src= "https://moretoexploreapistorage.blob.core.windows.net/site-images/about-me-img.jpg" alt="" />
-      </aside>
-    </div>
+    <v-container>
+      <v-row class="main-row">
+        <v-col class="text-cont" :cols="textColumnWidth">
+          <h2 class="local-title">{{ articleTitle }}</h2>
+          <div class="welcome-msg">
+            <p>{{ articleText }}</p>
+          </div>
+        </v-col>
+        <v-col class="img-cont" :cols="imageColumnWidth">
+          <img 
+            class="profile-pic"
+            src="https://moretoexploreapistorage.blob.core.windows.net/site-images/about-me-img.jpg"
+            alt=""
+          />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -23,6 +28,14 @@ export default {
       "Lorem ipsum dolor sit amet, molestie enim, nec viverra sapien lacus in massa. Vivamus sodales, lacus mattis tincidunt efficitur, erat nibh accumsan ex, et ornare ex ligula ut turpis. Sed interdum placerat lacus, nec fermentum augue auctor et.",
     articleTitle: "Hello Travelers!",
   }),
+  computed: {
+    textColumnWidth() {
+      return window.outerWidth > 600 ? 8 : 0;
+    },
+    imageColumnWidth() {
+      return window.outerWidth > 600 ? 4 : 0;
+    }
+  },
 };
 </script>
 
@@ -65,7 +78,31 @@ export default {
   max-height: 100%;
 }
 
-aside {
-  max-height: 100%;
+@media screen and (max-width: 600px) {
+  .intro-container {
+    height: 40rem;
+    flex-wrap: wrap;
+    text-align: center;
+  }
+
+  .main-row {
+    flex-direction: column;
+  }
+  
+  .text-cont, .img-cont {
+    width: 100%;
+    /* height: 45%; */
+  }
+
+  .img-cont {
+    padding: 0;
+  }
+
+  .profile-pic {
+    height: 100%;
+    width: 100%;
+  }
+
 }
+
 </style>
